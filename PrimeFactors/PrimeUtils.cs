3e26cdb52ce52d9a659d3e620e2f36acb7c1be3e
Slavko Krstic <slavkomae@gmail.com>
2015-09-07 14:25:30 -0400

    First commit

---
+++ PrimeFactors/PrimeUtils.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeFactors
+{
+    using System.ComponentModel;
+
+    public static class PrimeUtils
+    {
+        public static List<int> GetFactors(int n)
+        {
+            var retVal = new List<int>();
+
+            int newN = n;
+
+            while (newN != 1)
+            {
+                var lowestPrime = GetLowestPrime(newN, out newN);
+                retVal.Add(lowestPrime);
+            }
+
+
+            return retVal;
+        }
+
+        private static int GetLowestPrime(int n, out int newNumber)
+        {
+            double koren = Math.Sqrt(n);
+            var sqKoren = Convert.ToInt32(Math.Ceiling(koren));
+
+            for (int i = 2; i <= sqKoren; i++)
+            {
+
+                if (n % i == 0)
+                {
+
+                    newNumber = n / i;
+                    return i;
+
+                }
+
+
+            }
+
+            newNumber = 1;
+            return n;
+        }
+    }
+    
+
+}
---
+++ PrimeFactors/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeFactors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter number");
+            var input = Console.ReadLine();
+            int broj;
+            List<int> factors = null;
+            if (int.TryParse(input, out broj))
+            {
+                factors = PrimeUtils.GetFactors(broj);
+            }
+
+            foreach (var factor in factors)
+            {
+                Console.WriteLine(factor);
+            }
+
+            Console.WriteLine();
+
+
+
+        }
+    }
+}
